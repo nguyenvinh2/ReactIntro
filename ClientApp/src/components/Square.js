d57994171d86e4a07ab354c9feb7a0f298163933
nguye <nguyenv2@outlook.com>
2018-12-18 16:26:52 -0800

    Add project files.

---
+++ ClientApp/src/components/Square.js
@@ -0,0 +1,22 @@
+﻿import React, { Component } from 'react';
+import './TicTacToe.css';
+
+export class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
+  render() {
+    return (
+      <button className='square' onClick={function () {
+        this.setState({ value: 'X' });
+      }}
+      >
+        {this.state.value}
+      </button>
+    );
+  }
+}
