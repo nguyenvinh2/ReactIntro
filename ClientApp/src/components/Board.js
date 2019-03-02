1428500dc749a67bdcfa5b4c2bcf169d7481db7c
nguye <nguyenv2@outlook.com>
2019-03-01 16:15:36 -0800

    board constructor

--- ClientApp/src/components/Board.js
+++ ClientApp/src/components/Board.js
@@ -3,6 +3,12 @@ import { Square } from './Square';
 import './TicTacToe.css';
 
 export class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
   renderSquare(i) {
     return <Square value={i}/>;
   }
